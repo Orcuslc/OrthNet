0e64d79c196147174449e0a39dedc9a47638aa0e
orcuslc <orcuslc@hotmail.com>
2018-02-26 14:17:22 -0600

    add multi dimensional legendre polynomial for tensorflow

--- tensorflow/layer.py
+++ tensorflow/layer.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from ..utils.multi_dim import enumerate_dim
+
 
 def legendre_list(n, x):
 	"""
@@ -23,6 +25,7 @@ def legendre_list(n, x):
 		polys.append(((2*i+1)*tf.multiply(x, polys[-1])-i*polys[-2])/(i+1))
 	return polys
 
+
 def legendre_tensor(n, x):
 	"""
 	Generate 1-dimensional Legendre polynomial by three-term recursion:
@@ -44,4 +47,57 @@ def legendre_tensor(n, x):
 	polys = [tf.ones_like(x), x]
 	for i in range(1, n):
 		polys.append(((2*i+1)*tf.multiply(x, polys[-1])-i*polys[-2])/(i+1))
+	return tf.concat(polys, axis = 1)
+
+
+def multi_dim_legendre_list(n, var):
+	"""
+	multi dimensional legendre polynomials
+
+	input:
+		n: order of target polynomial
+		x: a list of tensors 
+
+	output:
+		y: a list of function values
+
+	>>> multi_dim_legendre_list(3, [x, y])
+	>>> [p3(x)p0(y), p2(x)p1(y), p1(x)p2(y), p0(x)p3(y)] # Each pi(x) is a legendre polynomail of order i, (a tensor)
+	"""
+	one_dim_polys, polys = [], []
+	for x in var:
+		one_dim_polys.append(legendre_list(n, x))
+	dim_combinations = enumerate_dim(n, len(var))
+	for comb in dim_combinations:
+		print(comb)
+		apoly = 1.
+		for i in range(len(comb)):
+			apoly = tf.multiply(apoly, one_dim_polys[i][comb[i]])
+		polys.append(apoly)
+	return polys
+
+
+def multi_dim_legendre_tensor(n, var):
+	"""
+	multi dimensional legendre polynomials
+
+	input:
+		n: order of target polynomial
+		x: a list of tensors 
+
+	output:
+		y: a tensor of function values
+
+	>>> multi_dim_legendre_list(3, [x, y])
+	>>> Tensor([p3(x)p0(y), p2(x)p1(y), p1(x)p2(y), p0(x)p3(y)]) # Each pi(x) is a legendre polynomail of order i, (a tensor)
+	"""
+	one_dim_polys, polys = [], []
+	for x in var:
+		one_dim_polys.append(legendre_list(n, x))
+	dim_combinations = enumerate_dim(n, len(var))
+	for comb in dim_combinations:
+		apoly = 1.
+		for i in range(len(comb)):
+			apoly = tf.multiply(apoly, one_dim_polys[i][comb[i]])
+		polys.append(apoly)
 	return tf.concat(polys, axis = 1)
