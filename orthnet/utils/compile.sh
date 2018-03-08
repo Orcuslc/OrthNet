353afccda0e67ab26802d274bda1072b12e1220a
orcuslc <orcuslc@hotmail.com>
2018-03-07 21:13:31 -0600

    recompile cpp with Ofast

--- orthnet/utils/compile.sh
+++ orthnet/utils/compile.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 swig -python -c++ enum_dim.i
-g++ -c -fPIC enum_dim_wrap.cxx -I/usr/include/python3.6
-g++ -shared enum_dim_wrap.o -o _enum_dim.so 
+g++ -Ofast -c -fPIC enum_dim_wrap.cxx -I/usr/include/python3.5
+g++ -Ofast -shared enum_dim_wrap.o -o _enum_dim.so 
