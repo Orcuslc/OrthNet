a5d2245d2c2a149d1f3160a2f8bfac42201a51e9
orcuslc <orcuslc@hotmail.com>
2018-03-11 01:15:06 -0500

    add demo of legendre

--- demo/legendre.py
+++ demo/legendre.py
@@ -5,14 +5,9 @@ import tensorflow as tf
 import torch
 from torch.autograd import Variable
 import numpy as np
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-# x0 = np.linspace(-1, 1, 1)
-# x_data = np.zeros((1, 2))
-# x_data[:, 0] = x0
-# x_data[:, 1] = x0
-x_data = np.array([[1., -1.], [1., -1.]])
+
+x_data = np.random.random((3, 2))
 x_tf = tf.placeholder(dtype = tf.float32, shape = (None, 2))
 x_torch = Variable(torch.Tensor(x_data))
 
@@ -21,12 +16,18 @@ y_torch = Legendre('pytorch', 2, x_torch)
 
 with tf.Session() as sess:
 	z_tf = sess.run(y_tf.tensor, feed_dict = {x_tf:x_data})
-	# print(sess.run(y_tf.list, feed_dict = {x_tf:x_data}))
-
 z_torch = y_torch.tensor.data.numpy()
 
+print('length:', y_tf.length)
+print('combination:', y_tf.combination)
+print('index:', y_tf.index)
+print('tf values:')
 print(z_tf)
+print('torch values:')
 print(z_torch)
+print('equal?')
+print(np.allclose(z_tf, z_torch))
+print('\n')
 
 y_tf.update(4)
 y_torch.update(4)
@@ -35,5 +36,13 @@ with tf.Session() as sess:
 	z_tf = sess.run(y_tf.tensor, feed_dict = {x_tf:x_data})
 z_torch = y_torch.tensor.data.numpy()
 
+print('updated:')
+print('length:', y_tf.length)
+print('combination:', y_tf.combination)
+print('index:', y_tf.index)
+print('tf values:')
 print(z_tf)
+print('torch values:')
 print(z_torch)
+print('equal?')
+print(np.allclose(z_tf, z_torch))
