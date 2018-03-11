c5fa7fdf1d00f32bc6f7ba92d5bc1aa7942d111c
orcuslc <orcuslc@hotmail.com>
2018-03-11 01:24:08 -0500

    add some demos

---
+++ demo/hermite.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.append('../')
+from orthnet import Hermite
+import tensorflow as tf
+import torch
+from torch.autograd import Variable
+import numpy as np
+
+
+x_data = np.random.random((3, 2))
+x_tf = tf.placeholder(dtype = tf.float32, shape = (None, 2))
+x_torch = Variable(torch.Tensor(x_data))
+
+y_tf = Hermite('tensorflow', 2, x_tf)
+y_torch = Hermite('pytorch', 2, x_torch)
+
+with tf.Session() as sess:
+	z_tf = sess.run(y_tf.tensor, feed_dict = {x_tf:x_data})
+z_torch = y_torch.tensor.data.numpy()
+
+print('length:', y_tf.length)
+print('combination:', y_tf.combination)
+print('index:', y_tf.index)
+print('tf values:')
+print(z_tf)
+print('torch values:')
+print(z_torch)
+print('equal?')
+print(np.allclose(z_tf, z_torch))
+print('\n')
+
+y_tf.update(4)
+y_torch.update(4)
+
+with tf.Session() as sess:
+	z_tf = sess.run(y_tf.tensor, feed_dict = {x_tf:x_data})
+z_torch = y_torch.tensor.data.numpy()
+
+print('updated:')
+print('length:', y_tf.length)
+print('combination:', y_tf.combination)
+print('index:', y_tf.index)
+print('tf values:')
+print(z_tf)
+print('torch values:')
+print(z_torch)
+print('equal?')
+print(np.allclose(z_tf, z_torch))
