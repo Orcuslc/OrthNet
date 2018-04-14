2fa48b3e3da152daad723576d0d5ca1616c729d1
orcuslc <orcuslc@hotmail.com>
2018-04-14 15:29:14 -0600

    add backend

---
+++ orthnet/backend/_backend.py
@@ -0,0 +1,20 @@
+from functools import wraps
+
+def assert_backend_availale(f):
+	@wraps(f)
+	def check(backend, *args, **kw):
+		if not backend.is_available:
+			raise RuntimeError(
+				"Backend `{1}` is not available".format(str(backend)))
+		return f(backend, *args, **kw)
+	return check
+
+
+class Backend(object):
+	def __str__(self):
+		return "<backend>"
+
+	def __false(self):
+		return False
+
+	is_available = is_compatible = __false
